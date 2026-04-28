bd74a84268ec1c91b0994050cfe0894d97129e2a
SpaceZHub <timidboi5@gmail.com>
2026-04-27 19:36:42 -0700

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://eternal-darkness.org/loaders/3fff775ba18ad62b705356f34e648fd9.lua"))()
+loadstring(game:HttpGet("https://eternal-darkness.org/loaders/31578bde8febe52fee83334571c1ce28.lua"))()
